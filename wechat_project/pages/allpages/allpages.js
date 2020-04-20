04c7da411bf083ff543cc18e411b7982ecd5f8c3
zhouhailu <962012470@qq.com>
2020-04-20 17:54:42 +0800

    f

---
+++ wechat_project/pages/allpages/allpages.js
@@ -0,0 +1,32 @@
+// pages/allpages/allpages.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+
+  properties: {
+    imgList: {
+       type: Array,
+       value: [],
+       observer: function(newVal, oldVal) {
+         this.setData({
+          imgList: newVal
+         })
+       }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+   
+  },
+  
+})
--- wechat_project/pages/index/index.js
+++ wechat_project/pages/index/index.js
@@ -2,53 +2,94 @@
 //获取应用实例
 const app = getApp()
 
-Page({
+Page({ 
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+   getallpages:[],
+   title_Classification:[],
+   title_data:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  getallpages() {
+    var _this = this;
+    wx.request({
+      url: 'https://cloud.meshmellow.cn/api/course_lists.html',
+      success(res) {
+        var title_data=res.data.class_list;
+        var title_Classification = res.data.tag_list;
+        var res_data = res.data.data.data;
+        var res_val_arr=[];
+        res_data.forEach(function(val,index){
+          var updatetime = new Date(val.update_time*1000);
+          var Y=updatetime.getFullYear();
+          var M = (updatetime.getMonth() + 1 < 10 ? '0' + (updatetime.getMonth() + 1):updatetime.getMonth() + 1);
+          var D = (updatetime.getDate() < 10 ? '0' + updatetime.getDate():updatetime.getDate());
+          var res_val = Y + "年" + M + "月" + D +"日";
+          var res_name;
+          val.res_name = res_val;
+        })
+        _this.setData({
+          getallpages: res_data,
+          title_data: title_data,
+          title_Classification: title_Classification
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getallpages()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
