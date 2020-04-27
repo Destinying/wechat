231eed6a8ab34dc4ff5d08fe2ecf13cf7de270e1
liyuanli <158096868@qq.com>
2020-04-27 11:22:55 +0800

    edit app.js and util.js

--- wechat_project/pages/mycode/mycode.js
+++ wechat_project/pages/mycode/mycode.js
@@ -1,17 +1,13 @@
 // pages/mycode/mycode.js
-<<<<<<< HEAD
 const app = new getApp();
 const myaudio = wx.createInnerAudioContext();
 
-=======
->>>>>>> d5dbe26a6c8e77a9d2fe44a02c323102de7379ea
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
     prompt: '',
     Base: '',
     defaultImg: '',
@@ -29,9 +25,6 @@ Page({
     is_login: 0,
     mask_status: 0,
     listClickFlag: 0,
-=======
-    
->>>>>>> d5dbe26a6c8e77a9d2fe44a02c323102de7379ea
   },
 
   /**
@@ -68,7 +61,6 @@ Page({
   onUnload: function () {
     
   },
-<<<<<<< HEAD
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
@@ -346,27 +338,4 @@ Page({
     this.setData({ isplay: false });
   },
 
-=======
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
->>>>>>> d5dbe26a6c8e77a9d2fe44a02c323102de7379ea
 })
