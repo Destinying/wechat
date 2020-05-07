3e68d59cb65adc047ad8b1ffff155b556130cd2d
zhouhailu <962012470@qq.com>
2020-05-07 09:55:10 +0800

    z

--- wechat_project/pages/confirmorder/confirmorder.js
+++ wechat_project/pages/confirmorder/confirmorder.js
@@ -5,14 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    radio_check:true,
+    orderid:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
+  get_order_list(){
+    var _this = this;
+    wx.request({
+      url: 'https://cloud.meshmellow.cn/wechatapi/confirmOrder.html',
+      method:"POST",
+      data:{
+        orderSn: _this.data.orderid
+      },
+      success(res){
+        console.log(res)
+      }
+    })
+  },
   onLoad: function (options) {
+    var _this=this;
     console.log(options)
+    _this.data.orderid = options.orderid
+    if(_this.data.orderid!=null){
+      _this.get_order_list()
+    }
   },
 
   /**
