185479e2f3b46ce358e6e7caa2271cd705765172
zhouhailu <962012470@qq.com>
2020-05-12 18:34:36 +0800

    z

--- wechat_project/pages/footprint/footprint.js
+++ wechat_project/pages/footprint/footprint.js
@@ -1,4 +1,4 @@
-// pages/record_pay/record_pay.js
+// pages/footprint/footprint.js
 const app=getApp()
 Page({
 
@@ -6,37 +6,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-    res:[],
-    uesrinfo:""
+    judgment_toggle:true,
+    hide_list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  record_pay:function(){
+  get_footprint(){
     var _this=this;
     wx.request({
-      url: 'https://cloud.meshmellow.cn/Wechatapi/get_record_pay',
+      url: 'https://cloud.meshmellow.cn/Wechatapi/get_record_video',
+      method:"POST",
       dataType:"json",
       data:{
-        token:app.globalData.token,
+        token:app.globalData.token
       },
-      method:"POST",
       success(res){
-        var res=res.data.data;
-        console.log(app.globalData.userInfo.level_name)
-        var uesrinfo = app.globalData.userInfo.level_name;
-        console.log(res);
+        var res=res.data.data.data
+        _this.data.hide_list=res;
+        _this.data.hide_list.forEach(function (val, index) {
+            val.slide_toggle = 0;
+        })
+        console.log(res)
         _this.setData({
-          res:res,
-          uesrinfo: uesrinfo
+          res:res
         })
       }
     })
   },
+  click_toggle(event){
+    var _this=this;
+    var currentTarget = event.currentTarget.id
+    var hide_list=_this.data.hide_list;
+    var slide_toggle1="";
+    var slide_toggle2 = "";
+    var slide_toggle=""
+    hide_list.forEach(function(val,index){
+      if (currentTarget==index){
+        val.slide_toggle = 1;
+      }else{
+        val.slide_toggle = 0;
+      }
+    })
+    console.log(hide_list)
+    _this.setData({
+      
+    })
+  },
   onLoad: function (options) {
     var _this=this;
-    _this.record_pay()
+    _this.get_footprint()
   },
 
   /**
@@ -50,7 +70,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
