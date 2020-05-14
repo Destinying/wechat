46f38858cb4328b0c1123ef476525f8d94b28886
zhouhailu <962012470@qq.com>
2020-05-14 18:44:17 +0800

    z

--- wechat_project/pages/mycode/mycode.js
+++ wechat_project/pages/mycode/mycode.js
@@ -25,7 +25,9 @@ Page({
     is_login: 0,
     mask_status: 0,
     listClickFlag: 0,
-    defalut_image:"../../image/person_image.png"
+    defalut_image:"../../image/person_image.png",
+    a:"",
+    a_0:""
   },
 
   /**
